24b253489ec6a06072fafe45a4bdaa6f6124b059
LucasLimaAZ <lucasdelimamonteiro@gmail.com>
2021-05-31 14:46:51 -0300

    improved error messages

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,95 +1,122 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { ProductForm } from '@shared/forms/products';
-import { ProductsService } from '@shared/services/products.service';
-import { CategoriesService } from '@shared/services/categories.service';
-import * as M from 'materialize-css';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup } from '@angular/forms'
+import { ProductForm } from '@shared/forms/products'
+import { ProductsService } from '@shared/services/products.service'
+import { CategoriesService } from '@shared/services/categories.service'
+import * as M from 'materialize-css'
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+	selector: 'app-products',
+	templateUrl: './products.component.html',
+	styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
 
-  public productForm: FormGroup;
-  public categories;
-  public autocomplete;
-  public aa;
-  public selectedCategory;
-  public loading: boolean = false;
+	public productForm: FormGroup
+	public categories: any
+	public autocomplete: any
+	public selectedCategory: any
+	public loading: boolean
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private service: ProductsService,
-    private categoriesService: CategoriesService,
-    private route: ActivatedRoute
-  ) {
+	constructor(
+		private formBuilder: FormBuilder,
+		private productService: ProductsService,
+		private categoriesService: CategoriesService,
+		private route: ActivatedRoute
+	) {}
 
-   }
+	ngOnInit() {
+		M.updateTextFields()
 
-  ngOnInit(): void {
-    M.updateTextFields();
+		this.productForm = this.formBuilder.group(new ProductForm)
+		this.getCategories()
+		let id = this.route.snapshot.paramMap.get('id')
 
-    this.productForm = this.formBuilder.group(new ProductForm);
-    this.getCategories();
-    if(this.route.snapshot.paramMap.get('id')) {
-      this.service.find(this.route.snapshot.paramMap.get('id'))
-        .subscribe(
-          (product) => {
-            this.productForm.patchValue(product);
-            M.updateTextFields();            
-            M.textareaAutoResize(document.querySelector('.materialize-textarea'));
-          },
-          error => M.toast({ html: error, classes:'fail'})
-        );
-    }
-  }
+		if (id) this.getProductInfo(id)
+	}
 
-  onSubmit() {
-    this.loading = true;
-    if(this.productForm.invalid) {
-      return;
-    }
+	onSubmit(): void {
+		this.loading = true
+			if(this.productForm.invalid) {
+			return
+		}
 
-    if(this.productForm.value.id) {
-      this.service.update(this.productForm.value)
-      .subscribe(
-        () => M.toast({html: "Atualizado com sucesso", classes:'succes'}),
-        error => M.toast({html: error, classes:'fail'}),
-        () => this.loading = false
-      )
-    } else {
-    this.service.store(this.productForm.value)
-      .subscribe(
-        () => {
-          M.toast({html: "Cadastrado com sucesso", classes:'succes'});
-          this.productForm.reset();
-        },
-        error => {
-          this.handleError(error);
-          this.loading = false;
-        },
-        () => this.loading = false
-      )
-    }
-  }
+		if(this.productForm.value.id) {
+			this.productService.update(this.productForm.value)
+				.subscribe(
+					() => M.toast({
+						html: "Atualizado com sucesso",
+						classes:'succes'
+					}),
+					error => M.toast({
+						html: "Ocorreu um erro ao atualizar o produto: " + error,
+						classes:'fail'
+					}),
+					() => this.loading = false
+				)
+		} else {
+			this.productService.store(this.productForm.value)
+				.subscribe(
+					() => {
+						M.toast({
+							html: "Cadastrado com sucesso",
+							classes:'succes'
+						})
+						this.productForm.reset()
+					},
+					error => {
+						M.toast({
+							html: "Ocorreu um erro ao cadastrar o produto: " + error,
+							classes:'fail'
+						})
+						this.handleError(error)
+						this.loading = false
+					},
+					() => this.loading = false
+				)
+		}
+	}
 
-  getCategories = () => this.categoriesService.get()
-  .subscribe(
-    (categories:any) =>  this.categories = categories,
-    error => M.toast({html: error, classes:'fail'})
-    );
+	getProductInfo = (id: any): void => {
+		this.productService.find(id)
+			.subscribe(
+				(product) => {
+					this.productForm.patchValue(product)
+					M.updateTextFields()
+					M.textareaAutoResize(
+						document.querySelector('.materialize-textarea')
+					)
+				},
+				error => M.toast({
+					html: "Ocorreu um erro ao buscar o produto: " + error,
+					classes:'fail'
+				})
+			)
+	}
 
-  handleError = (e) => {
-      const formFields = Object.keys(this.productForm.value);
-      const errorsFields = Object.keys(e);
-      let fields:any = formFields.filter(value => errorsFields.includes(value));
-      fields.map(f => this.productForm.controls[f].setErrors({'unavailable': true}))
-  }
+	getCategories = (): void => {
+		this.categoriesService.get()
+			.subscribe((categories:any) => {
+				this.categories = categories,
+				error => M.toast({
+					html: "Ocorreu um erro ao buscar as categorias: " + error,
+					classes:'fail'
+				})
+			})
+	}
 
-  clean = () => this.productForm.reset();
-  get formControls() { return this.productForm.controls; }
+	handleError = (e): void => {
+		const formFields = Object.keys(this.productForm.value)
+		const errorsFields = Object.keys(e)
+		let fields:any = formFields.filter(value => errorsFields.includes(value))
+		fields.map(f => this.productForm.controls[f].setErrors({'unavailable': true}))
+	}
+
+	clean = (): void => {
+		this.productForm.reset()
+	}
+
+	get formControls() { return this.productForm.controls }
 
 }
