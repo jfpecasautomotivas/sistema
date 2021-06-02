8a30f97f8c4512f0ff363e269572644fbee9c90a
Lucas de Lima Monteiro <lucasdelimamonteiro@gmail.com>
2021-06-01 23:20:12 -0300

    notifications fetch fixed at home screen

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,86 +1,74 @@
-import { Component, OnInit } from "@angular/core";
-import { NotificationsService } from "@shared/services/notifications.service";
-import { AuthenticationService } from "@shared/services/authentication.service";
-import { UserService } from "@shared/services/user.service";
-
-import * as M from "materialize-css";
-import { Router, Event, NavigationStart } from "@angular/router";
-import { take } from "rxjs/operators";
+import { Component, OnInit } from "@angular/core"
+import { NotificationsService } from "@shared/services/notifications.service"
+import { AuthenticationService } from "@shared/services/authentication.service"
+import { UserService } from "@shared/services/user.service"
+import * as M from "materialize-css"
 
 @Component({
-  selector: "app-navbar",
-  templateUrl: "./navbar.component.html",
-  styleUrls: ["./navbar.component.scss"],
+	selector: "app-navbar",
+	templateUrl: "./navbar.component.html",
+	styleUrls: ["./navbar.component.scss"],
 })
 export class NavbarComponent implements OnInit {
-  notifications = { data: [{ read_at: "" }] };
-  show = false;
-  clickOutCounter = 0;
-  loggedUserName = "";
+	public notifications: any = { data: [{ read_at: "" }] }
+	public show: boolean
+	public clickOutCounter:number = 0
+	public loggedUserName: string = ""
 
-  constructor(
-    private notificationsService: NotificationsService,
-    private authService: AuthenticationService,
-    private service: UserService,
-    private router: Router
-  ) {}
+	constructor(
+		private notificationsService: NotificationsService,
+		private authService: AuthenticationService,
+		private service: UserService
+	) {}
 
-  ngOnInit(): void {
-    this.service
-      .find(this.authService.currentUserValue?.id)
-      .subscribe((data) => {
-        console.log("entrou");
-        this.loggedUserName = data.name;
-      });
-    this.router.events.forEach((event) => {
-      const result =
-        location?.pathname.indexOf("login") > -1 ||
-        location?.pathname.indexOf("recovery-password") > -1;
-      if (event instanceof NavigationStart) {
-        if (!result) {
-          this.getNotifications();
-        }
-      }
-      // NavigationEnd
-      // NavigationCancel
-      // NavigationError
-      // RoutesRecognized
-    });
-  }
+	ngOnInit(): void {
+		this.getNotifications()
+		this.service.find(this.authService.currentUserValue?.id)
+			.subscribe((data) => this.loggedUserName = data.name)
+	}
 
-  showNotifications = () => (this.show = true);
+	showNotifications = () => (this.show = true)
 
-  hideNotifications = () => {
-    if (this.clickOutCounter === 0) {
-      this.sumClickOut();
-    } else {
-      this.show = false;
-      this.clickOutCounter = 0;
-    }
-  };
+	hideNotifications = () => {
+		if (this.clickOutCounter === 0) {
+			this.sumClickOut()
+		} else {
+			this.show = false
+			this.clickOutCounter = 0
+		}
+	}
 
-  sumClickOut = () => this.clickOutCounter++;
+	sumClickOut = () => this.clickOutCounter++
 
-  markAllAsRead = () => {
-    this.notificationsService
-      .marAsRead(this.authService.currentUserValue.id)
-      .subscribe(
-        () => this.getNotifications(),
-        (error) =>
-          M.toast({ html: "Erro ao buscar notificações", classes: "fail" })
-      );
-  };
+	markAllAsRead = () => {
+		this.notificationsService
+			.marAsRead(this.authService.currentUserValue.id)
+			.subscribe(
+				() => this.getNotifications(),
+				(error) => {
+					console.error(error)
+					M.toast({ 
+						html: "Erro ao buscar notificações", 
+						classes: "fail" 
+					})
+				}
+			)
+	}
 
-  getNotifications = () => {
-    this.notificationsService
-      .get(this.authService?.currentUserValue?.id)
-      .subscribe(
-        (data) => {
-          this.notifications = data;
-          this.show = false;
-        },
-        (error) =>
-          M.toast({ html: "Erro ao buscar notificações", classes: "fail" })
-      );
-  };
+	getNotifications = () => {
+		this.notificationsService.get(this.authService?.currentUserValue?.id)
+			.subscribe(
+				(data) => {
+					this.notifications = data
+					this.show = false
+				},
+				(error) => {
+					console.error(error)
+					M.toast({ 
+						html: "Erro ao buscar notificações", 
+						classes: "fail" 
+					})
+				}
+			)
+	}
 }
